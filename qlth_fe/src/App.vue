8f68466a9a20cd5ed0f6da688a49c7eb7225028c
HuyTo1368 <huyto1368@gmail.com>
2023-09-24 22:28:40 +0700

    create screen Login

--- qlth_fe/src/App.vue
+++ qlth_fe/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TheIndex />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheIndex from './pages/Login/MLogin.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheIndex
   }
 }
 </script>
 
 <style>
+@import url(../src/css/main.css);
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
---
+++ qlth_fe/src/components/base/HInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input :type="typeInput" class="m-input" :placeholder="placeHolder" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["placeHolder", "width", "typeInput"],
+};
+</script>
+
+<style scoped>
+.m-input {
+  height: 40px;
+  border: 1px solid #bbbbbb;
+  border-radius: 4px;
+  padding-left: 16px;
+  box-sizing: border-box;
+  width: 100%;
+}
+
+.m-input:focus {
+    outline: none;
+    border: 1px solid #019160;
+}
+</style>
